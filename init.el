3513f219f83ae7fe0d350899ecae247d2ac06f79
Steven Smith <stsmith@nabancard.com>
2018-09-25 12:53:08 -0400

    Adding random configs to emacs

--- init.el
+++ init.el
@@ -11,6 +11,13 @@
 (setq sentence-end-double-space nil)	; sentence SHOULD end with only a point.
 (setq default-fill-column 80)		; toggle wrapping text at the 80th character
 (setq initial-scratch-message "Welcome in Emacs") ; print a default message in the empty scratch buffer opened at startup
+(setq use-package-always-ensure t) ; Always ensure listed packages, why would i not want this? :D
+
+;; Set constants
+(defconst emacs-d
+  (file-name-directory
+   (file-chase-links load-file-name)))
+
 ;; Minimal UI
 (scroll-bar-mode -1)
 (tool-bar-mode   -1)
@@ -39,9 +46,22 @@
   (package-install 'use-package))
 (require 'use-package)
 
+(setq use-package-compute-statistics t)
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+)
+
+(use-package nlinum-relative
+  :config
+  (nlinum-relative-setup-evil)
+  (add-hook 'prog-mode-hook 'nlinum-relative-mode)
+  (setq nlinum-relative-current-symbol "")
+)
+
 ;; Vim mode
 (use-package evil
-  :ensure t
   :config
   (evil-mode 1)
   (setq-default evil-escape-delay 0.2)
@@ -49,45 +69,62 @@
   (evil-escape-mode)
 )
 
+(use-package powerline
+  :config
+  (powerline-default-theme)
+)
 ;; Evil Escape, map lk to escape
 (use-package evil-escape
-  :ensure t
   :init
   (setq-default evil-escape-key-sequence "lk")
 )
 
 ;; Theme
 (use-package doom-themes
-  :ensure t
   :config
   (load-theme 'doom-one t)
 )
 
+(use-package flycheck
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  (flycheck-add-mode 'javascript-eslint 'rjsx-mode)
+)
+
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
 ;; Helm
 (use-package helm
-  :ensure t
   :init
   (setq helm-M-x-fuzzy-match t
-  helm-mode-fuzzy-match t
-  helm-buffers-fuzzy-matching t
-  helm-recentf-fuzzy-match t
-  helm-locate-fuzzy-match t
-  helm-semantic-fuzzy-match t
-  helm-imenu-fuzzy-match t
-  helm-completion-in-region-fuzzy-match t
-  helm-candidate-number-list 150
-  helm-split-window-in-side-p t
-  helm-move-to-line-cycle-in-source t
-  helm-echo-input-in-header-line t
-  helm-autoresize-max-height 0
-  helm-autoresize-min-height 20)
+        helm-mode-fuzzy-match t
+        helm-buffers-fuzzy-matching t
+        helm-recentf-fuzzy-match t
+        helm-locate-fuzzy-match t
+        helm-semantic-fuzzy-match t
+        helm-imenu-fuzzy-match t
+        helm-completion-in-region-fuzzy-match t
+        helm-candidate-number-list 150
+        helm-split-window-in-side-p t
+        helm-move-to-line-cycle-in-source t
+        helm-echo-input-in-header-line t
+        helm-autoresize-max-height 0
+        helm-autoresize-min-height 20)
   :config
   (helm-mode 1)
 )
 
 ;; Which Key
 (use-package which-key
-  :ensure t
   :init
   (setq which-key-separator " ")
   (setq which-key-prefix-prefix "+")
@@ -96,40 +133,66 @@
 )
 
 ;; Counsel for various file searching things
-(use-package counsel :ensure t)
+(use-package counsel)
 (use-package helm-projectile
-  :ensure t
   :init
   (helm-projectile-on)
 )
 
 (use-package helm-swoop
-  :ensure t
   :config
   (setq helm-swoop-pre-input-function (lambda () ""))
 )
 
+(use-package symon
+  :config
+  (symon-mode)
+)
 (use-package helm-ag :ensure t)
 
-(use-package company :ensure t)
-(use-package rjsx-mode :ensure t)
-(add-to-list 'auto-mode-alist '(".js'" . rjsx-mode))
+(use-package company
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+)
+(use-package company-tern)
+
+(add-to-list 'company-backends 'company-tern)
+(add-hook 'rjsx-mode-hook (lambda () (tern-mode) (company-mode)))
+
+(use-package rjsx-mode
+  :config
+  (define-key rjsx-mode-map "<" nil)
+  (define-key rjsx-mode-map (kbd "C-d") nil)
+  (define-key rjsx-mode-map ">" nil)
+)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
+
+(use-package php-mode)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
 
 ;; Projectile
 (use-package projectile
-  :ensure t
   :init
   (setq projectile-require-project-root nil)
   :config
   (projectile-mode 1)
 )
 
-(use-package evil-magit :ensure t)
-(add-hook 'rjsx-mode-hook 'company-mode)
+(use-package eslint-fix)
+(use-package evil-magit)
 
+;; Custom functions
+(defun pdc/find-initfile ()
+  "Edit init.el in the current window."
+  (interactive)
+  (find-file-existing (expand-file-name "init.el" emacs-d)))
+(defun pdc/reload-initfile ()
+  "Reload init-real.el, in the current window."
+  (interactive)
+  (load-file (concat user-emacs-directory "init.el")))
 ;; Custom keybinding
 (use-package general
-  :ensure t
   :config (general-define-key
   :states '(normal visual insert emacs)
   :prefix "SPC"
@@ -138,9 +201,17 @@
   "SPC" '(helm-M-x :which-key "M-x")
   "/"   '(helm-swoop :which-key "find in the current buffer")
 
+  ;; Errors
+  "e"   '(:ignore t :which-key "Linting errors")
+  "el"  '(flycheck-list-errors :which-key "List errors")
+  "ev"  '(flycheck-verify-setup :which-key "Verify setup")
+  "ef"  '(eslint-fix :which-key "Fix errors in current file")
+
   ;; Files
   "f"   '(:ignore t :which-key "File")
   "ff"  '(helm-find-files :which-key "find files")
+  "fed" '(pdc/find-initfile :which-key "Open Init File")
+  "fer" '(pdc/reload-initfile :which-key "Reload File")
 
   ;; Project
   "p"   '(:ignore t :which-key "Project")
